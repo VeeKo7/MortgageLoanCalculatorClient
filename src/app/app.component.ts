d778dd96bf9ea01746d13665d58d826182e22f35
VeeKo7 <114544048+VeeKo7@users.noreply.github.com>
2023-03-28 07:42:43 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { HttpClient } from "@angular/common/http";
 import {Result} from "../model/result";
 import {catchError, Observable, retry} from "rxjs";
 
+/* The app is designed to allow users to 
+input their personal information, loan amount, 
+and loan period and to check if they 
+are approved for a loan. */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,9 +24,18 @@ export class AppComponent {
   approvedAmount: number = 0;
   resultIsBack: boolean = false;
 
+/* HTTP service injected in constructor */
   constructor(private http: HttpClient) {
   }
 
+/* App implements retry and catch error 
+logic in the pipe() method to handle any 
+errors that may occur during the HTTP request.
+The app sends a POST request to the 
+backend API at /loan/api/approveLoan 
+with the input data, and then subscribes 
+to the result. 
+*/
   submitUserProfile() {
 
     this.result = this.http.post<Result>(
